99815d464d43e8db93179090355d307f55b7e2df
Riku Walve <riku.walve@gmail.com>
2015-10-19 15:46:28 +0300

    fix coverage experiment

--- coverage.sh
+++ coverage.sh
@@ -16,14 +16,17 @@ OUTPUT=$OUTPUT_DIR/coverage
 LONGREADS=$ECOLI_LR
 REFERENCE=$ECOLI_REF
 
+# NOTE: sample-subset doesn't know of changes to this
+VALUES="25 50 75 100 150 175"
+
 # Generate random subsets
 mkdir -p $OUTPUT
 cd $OUTPUT
-python $SCRIPTS/sample-subset.py "$LONGREADS"
+python $SCRIPTS/sample-subset.py "$LONGREADS" "$REFERENCE"
 rm tmp.*.fastq
 
 # Run
-for i in 25 50 75 100 150 175; do
+for i in $VALUES; do
   mkdir -p $OUTPUT/lorma/$i
   cd $OUTPUT/lorma/$i
   $MASTER lorma $OUTPUT/subset_"$i"x.fasta
@@ -38,13 +41,13 @@ echo -e "Size\tAligned\tError rate\tIdentity\tExpCov\tObsCov\tElapsed time\t"\
 "CPU time\tMemory peak\tDisk peak\tSwap peak" | tee $OUTPUT/analysis.log
 
 echo -e "LoRDEC+LoRMA" | tee -a $OUTPUT/analysis.log
-for i in 25 50 75 100 150 175; do
+for i in $VALUES; do
   cd $OUTPUT/lorma/$i
   $ANALYZE -p corrected.fasta $OUTPUT/subset_"$i"x.fasta "$REFERENCE" stats.log disk.log stderr.log | tee -a $OUTPUT/analysis.log
 done
 
 echo -e "PBcR" | tee -a $OUTPUT/analysis.log
-for i in 25 50 75 100 150 175; do
+for i in $VALUES; do
   cd $OUTPUT/pbcr/$i
   $ANALYZE -p corrected.fasta $OUTPUT/subset_"$i"x.fasta "$REFERENCE" stats.log disk.log stderr.log | tee -a $OUTPUT/analysis.log
 done
